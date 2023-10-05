c3d4e009a8050838b64f93ab8682efea7eac8550
SirDest <akinro.destined@gmail.com>
2023-10-05 01:01:13 +0100

    Changes

--- src/components/WorkContainer.jsx
+++ src/components/WorkContainer.jsx
@@ -15,7 +15,7 @@ const WorkContainer = ({ works }) => {
       className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div duration-500 ease-in-out"
     >
       {/*Hover Effect */}
-      <div className="opacity-0 group-hover:opacity-100 duration-500 ease-in-out">
+      <div className="opacity-0 group-hover:opacity-100 duration-500 ease-in-out p-3">
         <span className=" text-2xl font-bold text-white tracking-wider">
           {name}
         </span>
